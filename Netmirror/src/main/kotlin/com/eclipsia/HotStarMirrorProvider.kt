c0d7e2f49273f7e5256216e926b08a6199213350
Eclipsia <novahaumea@proton.me>
2026-06-09 20:00:50 +0000

    Update HotStarMirrorProvider.kt

--- Netmirror/src/main/kotlin/com/eclipsia/HotStarMirrorProvider.kt
+++ Netmirror/src/main/kotlin/com/eclipsia/HotStarMirrorProvider.kt
@@ -1,9 +1,9 @@
 ﻿package com.eclipsia
 
 import android.content.Context
-import com.horis.eclipsia.entities.EpisodesData
-import com.horis.eclipsia.entities.PostData
-import com.horis.eclipsia.entities.SearchData
+import com.eclipsia.entities.EpisodesData
+import com.eclipsia.entities.PostData
+import com.eclipsia.entities.SearchData
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
